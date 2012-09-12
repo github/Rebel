8ef70f4ac6636db0e6247a84b258e4814c563247
joshaber <joshaber@gmail.com>
2012-09-12 10:20:03 -0400

    break RBLTestViewModel out into its own file

--- RebelTests/RBLTestViewModel.h
+++ RebelTests/RBLTestViewModel.h
@@ -10,4 +10,13 @@
 
 @interface RBLTestViewModel : RBLViewModel
 
+// Has the object been sent `-presentError:`?
+@property (nonatomic, readonly, assign) BOOL gotPresentError;
+
+// The argument passed in to `-someOtherThing:`.
+@property (nonatomic, readonly, strong) id argumentReceived;
+
+// Do some thing with the argument. Sets the `argumentReceived` property to `wat`.
+- (void)someOtherThing:(id)wat;
+
 @end
--- RebelTests/RBLTestViewModel.m
+++ RebelTests/RBLTestViewModel.m
@@ -8,6 +8,24 @@
 
 #import "RBLTestViewModel.h"
 
+@interface RBLTestViewModel ()
+@property (nonatomic, readwrite, assign) BOOL gotPresentError;
+@property (nonatomic, readwrite, strong) id argumentReceived;
+@end
+
 @implementation RBLTestViewModel
+
+#pragma mark RBLViewModel
+
+- (BOOL)presentError:(NSError *)error {
+	self.gotPresentError = YES;
+	return [super presentError:error];
+}
+
+#pragma mark API
+
+- (void)someOtherThing:(id)wat {
+	self.argumentReceived = wat;
+}
 
 @end
