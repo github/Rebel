665ff452f4d0a2ed0272a49137f6fc9126e4aa6c
Danny Greg <danny@github.com>
2012-09-13 14:52:57 +0100

    Add more extensive completionBlock testing.

--- RebelTests/CAAnimation+RBLBlockAdditionsSpec.m
+++ RebelTests/CAAnimation+RBLBlockAdditionsSpec.m
@@ -9,18 +9,23 @@
 SpecBegin(CAAnimationRBLBlockAdditions);
 
 __block CAAnimation *animation = nil;
+__block BOOL completionExecuted = NO;
+__block void(^completionBlock)() = ^ {
+	completionExecuted = YES;
+};
 
 before(^{
 	animation = [[CAAnimation alloc] init];
-	animation.rbl_completionBlock = ^ {
-		NSLog(@"Hello World!");
-	};
+	animation.rbl_completionBlock = completionBlock;
 	
 	expect(animation).toNot.beNil();
 });
 
 it(@"Should have set a completion block", ^ {
 	expect(animation.rbl_completionBlock).toNot.beNil();
+	expect(animation.rbl_completionBlock == completionBlock).to.beTruthy();
+	if (animation.rbl_completionBlock != nil) animation.rbl_completionBlock();
+	expect(completionExecuted).to.beTruthy();
 });
 
 it(@"Should have set itself has a delegate", ^{
