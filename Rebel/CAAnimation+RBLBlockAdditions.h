4ac9340eb40dcfa6cd70e9ffac0a832fd2e3ac04
Danny Greg <danny@github.com>
2012-09-13 11:13:16 +0100

    Stub in CAAnimation+Blocks

---
+++ Rebel/CAAnimation+RBLBlockAdditions.h
@@ -0,0 +1,13 @@
+//
+//  CAAnimation+RBLBlockAdditions.h
+//  Rebel
+//
+//  Created by Danny Greg on 13/09/2012.
+//  Copyright (c) 2012 GitHub. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface CAAnimation (RBLBlockAdditions)
+
+@end
---
+++ Rebel/CAAnimation+RBLBlockAdditions.m
@@ -0,0 +1,13 @@
+//
+//  CAAnimation+RBLBlockAdditions.m
+//  Rebel
+//
+//  Created by Danny Greg on 13/09/2012.
+//  Copyright (c) 2012 GitHub. All rights reserved.
+//
+
+#import "CAAnimation+RBLBlockAdditions.h"
+
+@implementation CAAnimation (RBLBlockAdditions)
+
+@end
