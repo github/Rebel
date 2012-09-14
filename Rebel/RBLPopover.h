760d06217da7608a95bb6a16796023aa34a34a49
Danny Greg <danny@github.com>
2012-09-14 12:28:58 +0100

    Document the main bulk of RBLPopover's functionality.

--- Rebel/RBLPopover.h
+++ Rebel/RBLPopover.h
@@ -37,12 +37,33 @@ typedef void (^RBLPopoverDelegateBlock)(RBLPopover *popover);
 @property (nonatomic, copy) RBLPopoverDelegateBlock willShowBlock;
 @property (nonatomic, copy) RBLPopoverDelegateBlock didShowBlock;
 
+// Designated initialiser.
 - (instancetype)initWithContentViewController:(NSViewController *)viewController;
 
+// Displays the popover
+//
+// positioningRect - The area which the popover should "cling" to. Given in
+//                   positioningView's coordinate space.
+// positioningView - The view which the positioningRect is relative to.
+// preferredEdge   - The edge of positioningRect which the popover should
+//                   "cling" to. If the entire popover will not fit on the
+//                   screen when clinging to the
+//                   preferredEdge another edge will be used to ensure the
+//                   content is visible. In the event that no edge allows the
+//                   popover to fit on the screen, preferredEdge is used.
 - (void)showRelativeToRect:(CGRect)positioningRect ofView:(NSView *)positioningView preferredEdge:(CGRectEdge)preferredEdge;
 
+// Closes the popover with the default fadeout duration (if the popover
+// animates).
 - (void)close;
+
+// Closes the popover with the given duration. If animates is set to NO the
+// popover closes immediately.
+//
+// duration - the duration of the fade animation.
 - (void)closeWithFadeoutDuration:(NSTimeInterval)duration;
+
+// Convenience method exposed for nib files.
 - (IBAction)performClose:(id)sender;
 
 @end
