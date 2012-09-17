68b14436c992e179d858033095a80d3165d2d760
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-09-16 22:23:12 -0700

    Note stupid scroller-disappearing behavior

--- Rebel/RBLClipView.h
+++ Rebel/RBLClipView.h
@@ -11,6 +11,11 @@
 //
 // A faster NSClipView based on CAScrollLayer.
 //
+// This view should be set as the scroll view's contentView as soon as possible
+// after the scroll view is initialized. For some reason, scroll bars will
+// disappear on 10.7 (but not 10.8) unless hasHorizontalScroller and
+// hasVerticalScroller are set _after_ the contentView.
+//
 @interface RBLClipView : NSClipView
 
 // The backing layer for this view.
