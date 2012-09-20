b04a04de9f876a46b3de63855d060982d0286818
Danny Greg <danny@github.com>
2012-09-20 11:48:48 -0700

    Revert "Revert "The background view becomes an NSView.""
    This reverts commit fadadb17dbbfa5ef72eb7107cf36572cdb15c272.

--- Rebel/RBLPopover.h
+++ Rebel/RBLPopover.h
@@ -117,7 +117,7 @@ typedef void (^RBLPopoverDelegateBlock)(RBLPopover *popover);
 
 @end
 
-@interface RBLPopoverBackgroundView : RBLView
+@interface RBLPopoverBackgroundView : NSView
 
 // Given a size of the content this should be overridden by subclasses to
 // describe how big the overall popover should be.
