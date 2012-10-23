b52ccce64278f4fd21b502d95af0fce73faa34e3
Jonathan Willing <jwilling@me.com>
2012-10-23 16:25:17 -0500

    add DS_Store to the gitignore

---
+++ Rebel/RBLTableCellView.h
@@ -0,0 +1,13 @@
+//
+//  RBLTableCellView.h
+//  Rebel
+//
+//  Created by Jonathan Willing on 10/23/12.
+//  Copyright (c) 2012 GitHub. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RBLTableCellView : NSTableCellView
+
+@end
---
+++ Rebel/RBLTableCellView.m
@@ -0,0 +1,28 @@
+//
+//  RBLTableCellView.m
+//  Rebel
+//
+//  Created by Jonathan Willing on 10/23/12.
+//  Copyright (c) 2012 GitHub. All rights reserved.
+//
+
+#import "RBLTableCellView.h"
+
+@implementation RBLTableCellView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
