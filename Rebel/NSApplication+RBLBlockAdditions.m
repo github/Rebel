56c87492997d29ca46b070c91f3bac8c726e1b1e
Jonathan Willing <jwilling@me.com>
2012-10-24 09:54:03 -0500

    add a sheet block category on NSApplication

---
+++ Rebel/NSApplication+RBLBlockAdditions.m
@@ -0,0 +1,38 @@
+//
+//  NSApplication+RBLBlockAdditions.m
+//  Rebel
+//
+//  Created by Jonathan Willing on 10/24/12.
+//  Copyright (c) 2012 GitHub. All rights reserved.
+//
+
+#import "NSApplication+RBLBlockAdditions.h"
+#import <objc/runtime.h>
+
+static void *RBLNSApplicationSheetBlockAssociatedObjectKey = &RBLNSApplicationSheetBlockAssociatedObjectKey;
+
+@implementation NSApplication (RBLBlockAdditions)
+
+- (void)beginSheet:(NSWindow *)sheet
+    modalForWindow:(NSWindow *)modalWindow
+ completionHandler:(void (^)(NSInteger returnCode))handler {
+	
+    [self beginSheet:sheet
+      modalForWindow:modalWindow
+       modalDelegate:self
+      didEndSelector:@selector(_sheetDidEnd:returnCode:contextInfo:)
+         contextInfo:NULL];
+    objc_setAssociatedObject(self, RBLNSApplicationSheetBlockAssociatedObjectKey, handler, OBJC_ASSOCIATION_COPY_NONATOMIC);
+}
+
+- (void)_sheetDidEnd:(NSWindow *)sheet
+          returnCode:(int)returnCode
+         contextInfo:(void *)contextInfo {
+    
+    void (^handler)(NSInteger returnCode) = objc_getAssociatedObject(self, &RBLNSApplicationSheetBlockAssociatedObjectKey);
+    [sheet orderOut:nil];
+    handler(returnCode);
+    objc_setAssociatedObject(self, &RBLNSApplicationSheetBlockAssociatedObjectKey, nil, OBJC_ASSOCIATION_COPY_NONATOMIC);
+}
+
+@end
