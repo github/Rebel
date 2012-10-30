818f6e017546f39977eca2046f8fb71d26f50489
Colin Wheeler <cocoasamurai@gmail.com>
2012-10-30 11:51:36 -0500

    initial support for swizzling the methods we'll need to use & implement custom_viewWillMoveToSuperView

--- Rebel/NSView+RBLViewControllerAdditions.m
+++ Rebel/NSView+RBLViewControllerAdditions.m
@@ -7,13 +7,15 @@
 //
 
 #import "NSView+RBLViewControllerAdditions.h"
+#import "RBLViewController.h"
+#import "NSObject+RBObjectSizzlingAdditions.h"
 #import <objc/runtime.h>
 
 void *kRBLViewControllerKey = &kRBLViewControllerKey;
 
-static BOOL hasSwizzledMethods = NO;
-
 @implementation NSView (NSView_RBLViewControllerAdditions)
+
+#pragma mark - ViewController
 
 -(id)viewController
 {
@@ -35,6 +37,72 @@ static BOOL hasSwizzledMethods = NO;
 		[self setNextResponder:self.viewController];
 		[self.viewController setNextResponder:ownResponder];
 	}
+}
+
+#pragma mark - Custom Methods
+
++(void)loadSupportForRBLViewControllers
+{
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		//swizzle swizzle...
+		[self swapMethod:@selector(viewWillMoveToSuperview:) with:@selector(custom_viewWillMoveToSuperview:)];
+        [self swapMethod:@selector(viewDidMoveToSuperview) with:@selector(custom_viewDidMoveToSuperview)];
+        
+        [self swapMethod:@selector(viewWillMoveToWindow:) with:@selector(custom_viewWillMoveToWindow:)];
+        [self swapMethod:@selector(viewDidMoveToWindow) with:@selector(custom_viewDidMoveToWindow)];
+        
+        [self swapMethod:@selector(setNextResponder:) with:@selector(custom_setNextResponder:)];
+	});
+}
+
+- (void)custom_viewWillMoveToSuperview:(NSView *)newSuperview
+{
+	[self custom_viewWillMoveToSuperview:newSuperview];
+	
+	if ([self.viewController isKindOfClass:[RBLViewController class]]) {
+		if (newSuperview == nil) {
+			[(RBLViewController *)self.viewController viewWillBeRemovedFromSuperview];
+			
+			if ((self.superview != nil) && (self.window != nil)) {
+				[(RBLViewController	*)self.viewController viewWillDisappear];
+			}
+		} else {
+			[(RBLViewController *)self.viewController viewWillMoveToSuperview:newSuperview];
+			
+			if (self.window != nil) {
+				[(RBLViewController *)self.viewController viewWillAppear];
+			}
+		}
+	}
+}
+
+- (void)custom_viewDidMoveToSuperview
+{
+	[self custom_viewDidMoveToSuperview];
+	
+	
+}
+
+- (void)custom_viewWillMoveToWindow:(NSWindow *)newWindow
+{
+	[self custom_viewWillMoveToWindow:newWindow];
+	
+	
+}
+
+- (void)custom_viewDidMoveToWindow
+{
+	[self custom_viewDidMoveToWindow];
+	
+	
+}
+
+- (void)custom_setNextResponder:(NSResponder *)newNextResponder
+{
+	[self custom_setNextResponder:newNextResponder];
+	
+	
 }
 
 @end
