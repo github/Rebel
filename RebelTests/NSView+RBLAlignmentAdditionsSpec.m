c2dc3e19dcb410f8c2467729cca98b11dd3e197e
Indragie Karunaratne <i@indragie.com>
2013-03-22 18:41:59 -0600

    Added unit test for NSView -rbl_viewBackingAlignedRect:alignmentOptions:

---
+++ RebelTests/NSView+RBLAlignmentAdditionsSpec.m
@@ -0,0 +1,20 @@
+//
+//  NSView+RBLAlignmentAdditionsSpec.m
+//  Rebel
+//
+//  Created by Indragie Karunaratne on 2013-03-22.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+SpecBegin(NSViewRBLAlignmentAdditions)
+
+describe(@"view alignment", ^{
+	it(@"should return a rect aligned to the view backing", ^{
+		NSWindow *window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0.f, 0.f, 100.f, 100.f) styleMask:NSTitledWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask backing:NSBackingStoreBuffered defer:NO];
+		NSRect nonAlignedRect = NSMakeRect(10.2f, 11.8f, 12.f, 13.f);
+		NSRect alignedRect = [window.contentView rbl_viewBackingAlignedRect:nonAlignedRect alignmentOptions:NSAlignAllEdgesNearest];
+		expect(NSEqualRects(alignedRect, NSMakeRect(10.f, 12.f, 12.f, 13.f))).to.beTruthy();
+	});
+});
+
+SpecEnd
