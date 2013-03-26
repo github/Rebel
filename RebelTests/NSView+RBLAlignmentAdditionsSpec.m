778c0283d084b56d9a73e5ac02310881661bb12d
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-03-25 17:47:49 -0700

    Test cleanup

--- RebelTests/NSView+RBLAlignmentAdditionsSpec.m
+++ RebelTests/NSView+RBLAlignmentAdditionsSpec.m
@@ -9,19 +9,26 @@
 SpecBegin(NSViewRBLAlignmentAdditions)
 
 describe(@"view alignment", ^{
+	NSRect nonAlignedRect = NSMakeRect(10.2, 11.8, 12, 13);
+	NSRect expectedRect = NSMakeRect(10, 12, 12, 13);
+
 	__block NSView *view;
+
 	before(^{
-		 view = [[NSView alloc] initWithFrame:NSMakeRect(20.f, 20.f, 20.f, 20.f)];
+		 view = [[NSView alloc] initWithFrame:NSMakeRect(20, 20, 20, 20)];
 	});
+
 	it(@"should return a rect aligned to the view backing", ^{
-		NSWindow *window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0.f, 0.f, 100.f, 100.f) styleMask:NSTitledWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask backing:NSBackingStoreBuffered defer:NO];
+		NSWindow *window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 100, 100) styleMask:NSTitledWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask backing:NSBackingStoreBuffered defer:NO];
 		[window.contentView addSubview:view];
-	});
-	it(@"should return a rect aligned to the view backing without a window.", ^{});
-	after(^{
-		NSRect nonAlignedRect = NSMakeRect(10.2f, 11.8f, 12.f, 13.f);
+
 		NSRect alignedRect = [view rbl_viewBackingAlignedRect:nonAlignedRect options:NSAlignAllEdgesNearest];
-		expect(NSEqualRects(alignedRect, NSMakeRect(10.f, 12.f, 12.f, 13.f))).to.beTruthy();
+		expect(NSEqualRects(alignedRect, expectedRect)).to.beTruthy();
+	});
+
+	it(@"should return a rect aligned to the view backing without a window", ^{
+		NSRect alignedRect = [view rbl_viewBackingAlignedRect:nonAlignedRect options:NSAlignAllEdgesNearest];
+		expect(NSEqualRects(alignedRect, expectedRect)).to.beTruthy();
 	});
 });
 
