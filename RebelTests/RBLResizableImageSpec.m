46d28a99fc6c2b83d91b0ef58ff724e17d7d28c1
Alan Rogers <alan@github.com>
2013-04-17 11:48:43 +1000

    Add tests to verify the correct asset it used
    @2x versus @1x

--- RebelTests/RBLResizableImageSpec.m
+++ RebelTests/RBLResizableImageSpec.m
@@ -10,28 +10,74 @@
 
 SpecBegin(RBLResizableImage)
 
-it(@"should use @2x assets correctly", ^{
+__block RBLResizableImage *testImage = nil;
+
+beforeEach(^{
 	NSURL *testImageURL = [[NSBundle bundleForClass:self.class] URLForResource:@"<RBLResizableImageSpec>_testimage" withExtension:@"tiff"];
 	
 	expect(testImageURL).toNot.beNil();
-	NSEdgeInsets insets = NSEdgeInsetsMake(0, 5, 0, 5);
 	
-	RBLResizableImage *testImage = [[[RBLResizableImage alloc] initByReferencingURL:testImageURL] rbl_resizableImageWithCapInsets:insets];
-	
+	testImage = [[RBLResizableImage alloc] initByReferencingURL:testImageURL];
+
 	expect(testImage).toNot.beNil();
+});
+
+it(@"should use @1x asset", ^{
+	CGSize targetSize = { .width = 50, .height = 26 };
 	
-	NSBitmapImageRep *bitmapImageRep = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:NULL pixelsWide:25 pixelsHigh:26 bitsPerSample:8 samplesPerPixel:4 hasAlpha:YES isPlanar:NO colorSpaceName:NSDeviceRGBColorSpace bitmapFormat:0 bytesPerRow:0 bitsPerPixel:0];
+	NSBitmapImageRep *bitmapImageRep = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:NULL pixelsWide:(NSInteger)targetSize.width pixelsHigh:(NSInteger)targetSize.height bitsPerSample:8 samplesPerPixel:4 hasAlpha:YES isPlanar:NO colorSpaceName:NSDeviceRGBColorSpace bitmapFormat:0 bytesPerRow:0 bitsPerPixel:0];
 	
 	NSGraphicsContext *context = [NSGraphicsContext graphicsContextWithBitmapImageRep:bitmapImageRep];
 	
 	[NSGraphicsContext setCurrentContext:context];
 	
-	[testImage drawInRect:(CGRect){ .origin = CGPointZero, .size = { .width = 25, .height = 26 } } fromRect:NSZeroRect operation:NSCompositeCopy fraction:1. respectFlipped:YES hints:nil];
+	[testImage drawInRect:(CGRect){ .origin = CGPointZero, .size = targetSize } fromRect:NSZeroRect operation:NSCompositeCopy fraction:1. respectFlipped:YES hints:nil];
 	
 	[context flushGraphics];
 	
-	[[bitmapImageRep TIFFRepresentation] writeToFile:[@"~/Desktop/test.tiff" stringByExpandingTildeInPath] atomically:YES];
+	NSUInteger testPixel[4];
+	
+	[bitmapImageRep getPixel:&testPixel[0] atX:24 y:10];
+	
+	NSUInteger red = testPixel[0];
+	NSUInteger green = testPixel[1];
+	NSUInteger blue = testPixel[2];
+	NSUInteger alpha = testPixel[3];
+	
+	// Should be a red pixel here if we used the @1x asset
+	expect(red).to.equal(255);
+	expect(green).to.equal(0);
+	expect(blue).to.equal(0);
+	expect(alpha).to.equal(255);
+});
 
+it(@"should use @2x asset", ^{
+	CGSize targetSize = { .width = 100, .height = 52 };
+	
+	NSBitmapImageRep *bitmapImageRep = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:NULL pixelsWide:(NSInteger)targetSize.width pixelsHigh:(NSInteger)targetSize.height bitsPerSample:8 samplesPerPixel:4 hasAlpha:YES isPlanar:NO colorSpaceName:NSDeviceRGBColorSpace bitmapFormat:0 bytesPerRow:0 bitsPerPixel:0];
+	
+	NSGraphicsContext *context = [NSGraphicsContext graphicsContextWithBitmapImageRep:bitmapImageRep];
+	
+	[NSGraphicsContext setCurrentContext:context];
+	
+	[testImage drawInRect:(CGRect){ .origin = CGPointZero, .size = targetSize } fromRect:NSZeroRect operation:NSCompositeCopy fraction:1. respectFlipped:YES hints:nil];
+	
+	[context flushGraphics];
+	
+	NSUInteger testPixel[4];
+	
+	[bitmapImageRep getPixel:&testPixel[0] atX:24 y:10];
+	
+	NSUInteger red = testPixel[0];
+	NSUInteger green = testPixel[1];
+	NSUInteger blue = testPixel[2];
+	NSUInteger alpha = testPixel[3];
+	
+	// Should be a green pixel here if we used the @1x asset
+	expect(red).to.equal(0);
+	expect(green).to.equal(255);
+	expect(blue).to.equal(0);
+	expect(alpha).to.equal(255);
 });
 
 SpecEnd
