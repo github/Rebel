d87df3f3b28bb70b3c565da5e63413aaa93d7c8c
joshaber <joshaber@gmail.com>
2013-08-14 17:46:41 -0400

    Be discriminating.

--- Rebel/RBLPopover.m
+++ Rebel/RBLPopover.m
@@ -289,7 +289,7 @@
 			RBLPopover *strongSelf = weakSelf;
 			static NSUInteger escapeKey = 53;
 			NSResponder *firstResponder = strongSelf.popoverWindow.firstResponder;
-			BOOL anythingInterestingAsFirstResponder = (firstResponder != self || firstResponder != nil);
+			BOOL anythingInterestingAsFirstResponder = [firstResponder isKindOfClass:NSTextView.class];
 			if (event.type == NSKeyUp && event.keyCode == escapeKey && (!anythingInterestingAsFirstResponder || strongSelf.behavior == RBLPopoverBehaviorTransient)) {
 				[strongSelf close];
 				return nil;
