b6e0c527f3af8381087e7033c22ff4c336e9a7f8
Matt Diephouse <matt@diephouse.com>
2014-02-24 14:09:24 -0500

    Add a demo app for RBLPopover

---
+++ Demos/RBLPopoverDemo/RBLPopoverDemo/RPDContentViewController.m
@@ -0,0 +1,19 @@
+//
+//  RPDContentViewController.m
+//  RBLPopoverDemo
+//
+//  Created by Matt Diephouse on 2/24/14.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+#import "RPDContentViewController.h"
+
+@implementation RPDContentViewController
+
+#pragma mark - NSObject
+
+- (id)init {
+	return [self initWithNibName:NSStringFromClass(self.class) bundle:nil];
+}
+
+@end
---
+++ Demos/RBLPopoverDemo/RBLPopoverDemo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RBLPopoverDemo
+//
+//  Created by Matt Diephouse on 2/24/14.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
