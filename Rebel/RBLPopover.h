015ba1ac568ef5f4a36660f59e1f7b2f74fc0ee9
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-07-16 10:55:43 -0700

    Remove RBLView and RBLViewController imports

--- Rebel/RBLPopover.h
+++ Rebel/RBLPopover.h
@@ -7,7 +7,6 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <Rebel/RBLView.h>
 
 @class RBLPopover;
 @class RBLPopoverBackgroundView;
--- Rebel/Rebel.h
+++ Rebel/Rebel.h
@@ -6,24 +6,22 @@
 //  Copyright (c) 2012 GitHub. All rights reserved.
 //
 
+#import <Rebel/CAAnimation+RBLBlockAdditions.h>
 #import <Rebel/NSApplication+RBLBlockAdditions.h>
 #import <Rebel/NSAttributedString+RBLHTMLAdditions.h>
 #import <Rebel/NSColor+RBLCGColorAdditions.h>
 #import <Rebel/NSFont+RBLFallbackAdditions.h>
 #import <Rebel/NSImage+RBLResizableImageAdditions.h>
-#import <Rebel/NSView+RBLAnimationAdditions.h>
 #import <Rebel/NSView+RBLAlignmentAdditions.h>
+#import <Rebel/NSView+RBLAnimationAdditions.h>
 #import <Rebel/RBLClipView.h>
-#import <Rebel/RBLResizableImage.h>
-#import <Rebel/RBLView.h>
-#import <Rebel/CAAnimation+RBLBlockAdditions.h>
+#import <Rebel/RBLExpandingContainerView.h>
+#import <Rebel/RBLHTMLView.h>
+#import <Rebel/RBLOutlineView.h>
 #import <Rebel/RBLPopover.h>
-#import <Rebel/RBLViewController.h>
-#import <Rebel/RBLTableCellView.h>
+#import <Rebel/RBLResizableImage.h>
 #import <Rebel/RBLScrollView.h>
 #import <Rebel/RBLShadowedTextFieldCell.h>
-#import <Rebel/RBLHTMLView.h>
-#import <Rebel/RBLTableView.h>
-#import <Rebel/RBLExpandingContainerView.h>
 #import <Rebel/RBLSlidingContainerView.h>
-#import <Rebel/RBLOutlineView.h>
+#import <Rebel/RBLTableCellView.h>
+#import <Rebel/RBLTableView.h>
